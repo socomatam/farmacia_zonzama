ea4a9d455c59c625cc621f7e773e28742b30ff35
Carlos <satizabal44@gmail.com>
2020-01-31 13:29:04 +0000

    Vista de login creada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,14 +3,14 @@
 	
 
 	<div class="card-body">
-		<div class="card-header">{{ __('Login') }}</div>
+		<div> <span>Entrar</span></div>
 		<form method="POST" action="{{ route('login') }}">
 			@csrf
 
 			<div class="form-group row">
-				<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+				<label for="email" class="login_label">{{ __('E-Mail Address') }}</label>
 
-				<div class="col-md-6">
+				<div class="login_input">
 					<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> @error('email')
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $message }}</strong>
@@ -20,9 +20,9 @@
 			</div>
 
 			<div class="form-group row">
-				<label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+				<label for="password" class="login_label">{{ __('Password') }}</label>
 
-				<div class="col-md-6">
+				<div class="login_input">
 					<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"> @error('password')
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $message }}</strong>
@@ -44,7 +44,7 @@
 			</div>
 
 			<div class="form-group row mb-0">
-				<div class="col-md-8 offset-md-4">
+				<div class="login_btns">
 					<button type="submit" class="btn btn-primary">
 						{{ __('Login') }}
 					</button>
@@ -61,9 +61,13 @@
 		</form>
 	</div>
 	
-	<div>
+	<div class="login_register">
+		
+		<span>¿Nuevo cliente?</span>
+		<br><br><br><br>
 		<button>
-			<a href="#">Registrase</a>
+			
+			<a href="{{url('/register')}}">Registrase</a>
 			
 		</button>
 	</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.auth')
 @section('content')
-<div class="container">
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -72,5 +72,5 @@
             </div>
         </div>
     </div>
-</div>
+
 @endsection
