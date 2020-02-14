0bfbe037900f493772044a4bda130107171f38f1
Carlos <satizabal44@gmail.com>
2020-02-14 19:33:57 +0000

    Vue

---
+++ public/assets/js/vue.js
@@ -0,0 +1,10 @@
+window.onload = function() { 
+	var appContact = new Vue ({ 
+		el: '#app_contact', 
+		data: { 
+			address: 'Calle Dr. Barraquer, 6, 35500 Arrecife, Las Palmas', 
+			email: 'cifpzonzamas@mail.com', 
+			phone: '928 81 31 14', 
+		},//end data
+	});//end appContact 
+};
